0f31e0d3dd894a6bd2f9175f86fcd8281d33f292
David Wittman <david@wittman.com>
2011-09-20 18:29:29 -0500

    Made it more classy

--- apdebug.py
+++ apdebug.py
@@ -21,130 +21,139 @@
 
 """
 
-from glob import glob
 import os
 import socket
 import re
-from subprocess import Popen, PIPE
 import sys
+from glob import glob
 from time import sleep
+from subprocess import Popen, PIPE
 
-def open_connection():
-    """Open a connection with the webserver so we can grab the PID"""
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(('localhost', 80))
-    except socket.error, e:
-        sys.stderr.write("[ERROR] %s\n" %e[1])
-        sys.exit(1)
-    sock.send("HEAD / HTTP/1.1\r\nHost: localhost\r\nConnection: Keep-Alive\r\n\r\n")
-    response = sock.recv(2048)
+class apdebug(object):
+    def __init__(self, url):
+        url = url.split('/')
+        self.hostname = url.pop(0)
+        if self.hostname.startswith("http"): 
+            del(url[0])
+            self.hostname = url.pop(0)
+        self.request = '/' + '/'.join(url)
 
-    return sock
+    class colors(object):
+        RED="\033[1;31m"
+        RESET="\033[0m"
 
-def _netstat():
-    """Grab the open connections from proc"""
-    PROCS = ( '/proc/net/tcp', '/proc/net/tcp6' )
-    output = []
-
-    for procfile in PROCS:
+    def open_connection(self):
+        """Open a connection with the webserver so we can grab the PID"""
         try:
-            fd = open(procfile, 'r')
-        except:
-            return None
-        else:
-            content = fd.readlines()
-            content.pop(0)
-            fd.close()
-            output.extend(content)
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect(('localhost', 80))
+        except socket.error, e:
+            sys.stderr.write("[ERROR] %s\n" %e[1])
+            sys.exit(1)
+        self.sock.send("HEAD / HTTP/1.1\r\nHost: localhost\r\nConnection: Keep-Alive\r\n\r\n")
+        response = self.sock.recv(2048)
 
-    return [ line.split() for line in output ]
+        return response
 
-def _get_pid_of_inode(inode):
-    """Find the pid using inode and return it"""
-    for i in glob('/proc/[0-9]*/fd/[0-9]*'):
-        try:
-            if re.search(inode, os.readlink(i)):
-                return i.split('/')[2]
-        except OSError:
-            pass
+    def _netstat(self):
+        """Grab the open connections from proc"""
+        PROCS = ( '/proc/net/tcp', '/proc/net/tcp6' )
+        output = []
 
-def get_listener_pid(localport):
-    """Find the process ID for the service accepting our connection"""
-    localport = hex(localport)[2:].upper()
+        for procfile in PROCS:
+            try:
+                fd = open(procfile, 'r')
+            except:
+                return None
+            else:
+                content = fd.readlines()
+                content.pop(0)
+                fd.close()
+                output.extend(content)
 
-    # Find the inode associated with our connection
-    for line in _netstat():
-        if line[2].split(':')[-1] == localport:
-            inode = line[9]
+        return [ line.split() for line in output ]
 
-    return _get_pid_of_inode(inode)
+    def _get_pid_of_inode(self, inode):
+        """Find the pid using inode and return it"""
+        for i in glob('/proc/[0-9]*/fd/[0-9]*'):
+            try:
+                if re.search(inode, os.readlink(i)):
+                    return i.split('/')[2]
+            except OSError:
+                pass
 
-def strace(pid, outfile):
-    """Start the strace"""
-    p = Popen(["strace", "-o", outfile, "-rfs", "512", "-p", pid], stderr=open("/dev/null",'w'))
-    return p.pid
+    def get_listener_pid(self):
+        """Find the process ID for the service accepting our connection"""
+        localport = self.sock.getsockname()[1]
+        localport = hex(localport)[2:].upper()
 
-def send_request(sock, url, headers=None):
-    sock.send("GET %s HTTP/1.1\r\nHost: %s\r\n\r\n" % (url, 'localhost'))
-    response = sock.recv(1024)
-    sock.close()
-    return response
+        # Find the inode associated with our connection
+        for line in self._netstat():
+            if line[2].split(':')[-1] == localport:
+                inode = line[9]
 
-# TODO: Nicer output
-def find_problems(outfile, n=5):
-    """Find the n slowest system calls"""
-    n = n * -1
-    output = open(outfile, 'r')
-    calls = output.readlines()
-    # Find the amount of time that the strace slept for before we made the call
-    sleepytime = calls[1].split()[1]
+        return self._get_pid_of_inode(inode)
 
-    # Sort the list of calls on the relative time, and find the top n
-    sorted_calls = sorted(calls, key=lambda x: x.split()[1])
-    top_times = [ row.split()[1] for row in sorted_calls[n:] ]
+    def _strace(self, pid):
+        """System call to strace"""
+        p = Popen(["strace", "-o", self.outfile, "-rfs", "512", "-p", pid], stderr=open("/dev/null",'w'))
+        return p.pid
 
-    # Replace our sleep time in the top times
-    if sleepytime in top_times:
-        top_times[top_times.index(sleepytime)] = sorted_calls[n-1].split()[1]
+    def strace(self, outfile):
+        """Start the strace"""
+        self.outfile = outfile
+        self.open_connection()
+        pid = self.get_listener_pid()
+        stpid = self._strace(pid)
+        sleep(.5)
+        self.send_request(self.request)
+        # Send SIGTERM to our strace process
+        os.kill(stpid, 15)
+        self.find_problems()
 
-    """ Find the system calls which correspond with the longest delays.
-        The relative times in strace correspond with the previous system call
-        WARNING: not very Pythonic """
-    top_calls = [ (j.split()[1], calls[i-1].split()[2:]) for i, j in enumerate(calls) if j.split()[1] in top_times ]
+    def send_request(self, url, headers=None):
+        self.sock.send("GET %s HTTP/1.1\r\nHost: %s\r\n\r\n" % (url, self.hostname))
+        response = self.sock.recv(1024)
+        self.sock.close()
+        return response
 
-    # Reverse sort our new list by the times
-    top_calls.sort(key=lambda x: x[0])
-    top_calls.reverse()
+    # TODO: Nicer output
+    def find_problems(self, n=5):
+        """Find the n slowest system calls"""
+        n = n * -1
+        output = open(self.outfile, 'r')
+        calls = output.readlines()
+        # Find the amount of time that the strace slept for before we made the call
+        sleepytime = calls[1].split()[1]
 
-    # Print
-    for time, call in top_calls:
-        print colors.RED + time + colors.RESET + ' ' + ' '.join(call)
+        # Sort the list of calls on the relative time, and find the top n
+        sorted_calls = sorted(calls, key=lambda x: x.split()[1])
+        top_times = [ row.split()[1] for row in sorted_calls[n:] ]
 
-class colors(object):
-    RED="\033[1;31m"
-    RESET="\033[0m"
+        # Replace our sleep time in the top times
+        if sleepytime in top_times:
+            top_times[top_times.index(sleepytime)] = sorted_calls[n-1].split()[1]
 
-def usage():
-    print "usage: " + sys.argv[0] + " /url/to/strace.php"
+        """ Find the system calls which correspond with the longest delays.
+            The relative times in strace correspond with the previous system call
+            WARNING: not very Pythonic """
+        top_calls = [ (j.split()[1], calls[i-1].split()[2:]) for i, j in enumerate(calls) if j.split()[1] in top_times ]
 
-def main():
-    outfile = '/tmp/strace.out'
+        # Reverse sort our new list by the times
+        top_calls.sort(key=lambda x: x[0])
+        top_calls.reverse()
 
-    if len(sys.argv) is not 2:
-        usage()
-        sys.exit(1)
+        # Print
+        for time, call in top_calls:
+            print self.colors.RED + time + self.colors.RESET + ' ' + ' '.join(call)
 
-    url = sys.argv[1]
-    sock = open_connection()
-    pid = get_listener_pid(sock.getsockname()[1])
-    stpid = strace(pid, outfile)
-    sleep(.5)
-    send_request(sock, url)
-    # Send sigterm to our strace process
-    os.kill(stpid,15)
-    find_problems(outfile)
+    @classmethod
+    def usage(self):
+        print "usage: " + sys.argv[0] + " /url/to/strace.php"
 
 if __name__ == '__main__':
-    main()
-
+    if len(sys.argv) is not 2:
+        apdebug.usage()
+        sys.exit(1)
+    ad = apdebug(sys.argv[1])
+    ad.strace('/tmp/strace.out')
